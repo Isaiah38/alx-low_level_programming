53f0701f4c84a89a9857b01f1e8b98670d8b36e4
Isaiah38 <brightlight882@gmail.com>
2022-03-10 10:08:53 -0800

    My second program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - display a message
+ * Return: display the message
+ */
+int main(void)
+
+{
+
+	char comment[] = "with proper grammar, but the outcome is a piece of art";
+
+	printf("%s/n", comment);
+
+	return (0);
+}
