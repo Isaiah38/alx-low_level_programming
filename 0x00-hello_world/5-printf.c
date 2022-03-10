cab45c11e02139bb2f8646884450a1e2d3db2e70
Isaiah38 <brightlight882@gmail.com>
2022-03-10 10:11:53 -0800

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@ int main(void)
 
 {
 
-	char comment[] = "with proper grammar, but the outcome is a piece of art";
+	char comment[] = "with proper grammar, but the outcome is a piece of art,";
 
 	printf("%s\n", comment);
 
