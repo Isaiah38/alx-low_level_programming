5fcfd437adbec656ecb9f6229f251f2793e29429
Isaiah38 <brightlight882@gmail.com>
2022-03-16 14:37:27 -0700

    checking 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - writes the character c to stdout
- *
- * Return: Always (0) when successful
- **/
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar\n");
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
 
-	return (0);
+        return (0);
 }
+
