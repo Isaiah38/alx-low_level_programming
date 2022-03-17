b58968f41b288b81a61be7b164ba89f4a526d693
Isaiah38 <brightlight882@gmail.com>
2022-03-17 08:52:58 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * print_numbers - print numbers 
- *
+ *@c: the value being checked
  * Return:  Always (0) when successful
  */
 void print_numbers(void)
@@ -10,8 +10,6 @@ void print_numbers(void)
 	int c;
 
 	for (c = 0; c <= 9; c++)
-	{
 		_putchar(c);
-	}
 	_putchar('\n');
 }
