d19fd4da1d8806571de10b1705d34a1605c49a33
Isaiah38 <brightlight882@gmail.com>
2022-03-18 14:32:27 -0700

    add and created new 0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-void positive_or_negative(void);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+void positive_or_negative(void);
 #endif
