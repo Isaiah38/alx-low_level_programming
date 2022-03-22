b8d253fb03bc453221210811603a07e48f50e474
Isaiah38 <brightlight882@gmail.com>
2022-03-21 17:42:43 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts - prints a string, followed by a new line, to stdout.
+ * @str: function parameter
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
