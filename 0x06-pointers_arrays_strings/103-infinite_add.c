5dcce26bdd20a337ac3e9de2c181a16542b20446
Isaiah38 <brightlight882@gmail.com>
2022-03-24 16:06:40 -0700

    103-infinite_add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,13 +6,11 @@
  * @n2: number to be added
  * @r: the result
  * @ size_r; size of the result
- * Return: 0 if positive
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j;
-	size_r = sizeof(*r);
 
 	for (i = 0; n1[i] != '\0'; i++)
 	{
@@ -23,9 +21,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			j++;
 		}
 		n1[i] = '\0';
+		r[] = n1;
 	}
-
-	if (*r >= 0)
-		return (0);
+	size_r = sizeof(r);
 }
 
